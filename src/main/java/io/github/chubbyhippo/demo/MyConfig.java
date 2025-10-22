257d5f0e9b5abc848d0bbb3bda5546fe5472c7c2
chubbyhippo <mkmsacc@outlook.com>
2025-10-22 21:57:36 +0700

    Add ApplicationRunner bean to MyConfig for logging configuration properties

--- src/main/java/io/github/chubbyhippo/demo/MyConfig.java
+++ src/main/java/io/github/chubbyhippo/demo/MyConfig.java
@@ -1,7 +1,24 @@
 package io.github.chubbyhippo.demo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyConfig {
+
+    @Value("${my.config.name}")
+    private String myConfigName;
+
+    @Value("${my.config.port}")
+    private Integer myConfigPort;
+
+    @Bean
+    ApplicationRunner init() {
+        return args -> {
+            System.out.println("my config name: " + myConfigName);
+            System.out.println("my config port: " + myConfigPort);
+        };
+    }
 }
